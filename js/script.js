305a26980a3ed265ab4000eb45694dc79c29c6b6
MonoDG <monodg@proton.me>
2023-10-09 17:38:43 -0400

    Add marker in game array for each player, switch turns

--- js/script.js
+++ js/script.js
@@ -35,3 +35,46 @@ const gameboard = (function () {
     return { getMarker1, getMarker2, getArray, addValue };
 
 })();
+
+function createPlayer(name, marker) {
+    let score = 0;
+    const getName = function () {
+        return name;
+    }
+    const getMarker = function () {
+        return marker;
+    }
+    const getScore = function () {
+        return score;
+    }
+    const incrementScore = function () {
+        score++;
+    }
+    const resetScore = function () {
+        score = 0;
+    }
+    return { getName, getMarker, getScore, incrementScore, resetScore };
+}
+
+const gameController = (function () {
+    const player1 = createPlayer("Player 1", gameboard.getMarker1());
+    const player2 = createPlayer("Player 2", gameboard.getMarker2());
+
+    let currentPlayer = (Math.random() > 0.5) ? player1 : player2;
+
+    const switchPlayer = function () {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+
+    const playRound = function () {
+        console.log(`Current turn: ${currentPlayer.getName()}`);
+        console.table(gameboard.getArray());
+        let position = prompt("Which x, y positions would you like to play (x, y)?: ")
+        let [posX, posY] = position.split(",");
+        gameboard.addValue(currentPlayer.getMarker(), posX, posY);
+        console.table(gameboard.getArray());
+        switchPlayer();
+    }
+
+    return { playRound, switchPlayer };
+})();
